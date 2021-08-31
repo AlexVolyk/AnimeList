0f067bf44cf1ed4d4953f4d6ac3cdd4225c723d2
AlexVolyk <85078173+AlexVolyk@users.noreply.github.com>
2021-09-01 00:02:07 +0300

    firstss

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const fileUpload = require('express-fileupload');
+// const fileUpload = require('express-fileupload');
 const Express = require('express');
 const app = Express();
 
@@ -8,20 +8,20 @@ const controllers = require('./controllers');
 const middleware = require('./middleware');
 
 app.use(middleware.CORS)
-app.use(fileUpload())
+// app.use(fileUpload())
 app.use(Express.json())
 
 
-// dbConnetcion.authenticate()
-//     .then(() => dbConnetcion.sync())
-//     .then(() => {
-//         app.listen(process.env.PORT, () => {
-//             console.log(`[SERVER]: App is listening on ${process.env.PORT}`)
-//         })
-//     })
-//     .catch((err) => {
-//         console.log(`[SERVER]: crashed ${err}`)
-//     })
+dbConnetcion.authenticate()
+    .then(() => dbConnetcion.sync())
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`[SERVER]: App is listening on ${process.env.PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log(`[SERVER]: crashed ${err}`)
+    })
 
     
 app.use('/user', controllers.userController);
@@ -29,7 +29,7 @@ app.use('/admin', controllers.adminController);
 app.use('/anime', controllers.animeController);
 
 
-app.listen(process.env.PORT, () => {
-    console.log(`[SERVER]: App is listening on ${process.env.PORT}`)
-});
+// app.listen(process.env.PORT, () => {
+//     console.log(`[SERVER]: App is listening on ${process.env.PORT}`)
+// });
 
